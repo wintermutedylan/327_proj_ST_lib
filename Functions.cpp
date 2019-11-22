5659e5b4b5cf8c8d0f3bd425a319fa024eb59f7e
Dylan Wintermute <dylan.wintermute@cnu.edu>
2019-11-22 18:55:16 -0500

    workign on it now.  error in function.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -15,13 +15,39 @@
 #include "./includes/Watch.h"
 #include "./includes/constants.h"
 
+using namespace std;
+
 //create a vector with appropriate numbers of Smalltalk_Brit,Smalltalk_American and ST_American_DonutEnthusiest
 //objects using unique pointers.  Since we are using c++11 returning this vector by value is fine since the 
 //compiler will move the vector on return rather than recreate it (this means there is no copy penalty)
 std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit,
 		int numAmerican, int numbAmericanDonutEnthusiest,
 		int numWatches) {
-	
+	std::vector<std::unique_ptr<Smalltalk>> smalltalkPointers;
+
+	for (int i = 0; i < numBrit; ++i){
+		std::unique_ptr<Smalltalk> a(new Smalltalk_Brit(i));
+		smalltalkPointers.push_back(a);
+	}
+	for (int i = 0; i < numAmerican; ++i){
+			std::unique_ptr<Smalltalk> a(new Smalltalk_American(i));
+			smalltalkPointers.push_back(a);
+		}
+	for (int i = 0; i < numbAmericanDonutEnthusiest; ++i){
+			std::unique_ptr<Smalltalk> a(new ST_American_DonutEnthusiest(i));
+			smalltalkPointers.push_back(a);
+		}
+	for (int i = 0; i < numbAmericanDonutEnthusiest; ++i){
+				std::unique_ptr<Smalltalk> a(new ST_American_DonutEnthusiest(i));
+				smalltalkPointers.push_back(a);
+			}
+	for (int i = 0; numWatches > 0 && i > smalltalkPointers.size(); ++i){
+					std::unique_ptr<Smalltalk> a(new ST_American_DonutEnthusiest(i));
+					smalltalkPointers.push_back(a);
+					if (smalltalkPointers[i]->giveWatch){
+						numWatches--;
+					}
+				}
 	//create a vector to hold SmallTalk unique pointers
 
 		//add brits to vector
@@ -36,4 +62,5 @@ std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit,
 		//from this function(see Smalltalk header for hints)
 
 		//return your vector
+	return smalltalkPointers;
 }
